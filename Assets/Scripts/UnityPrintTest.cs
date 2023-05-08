08ffc1210ef8783d29fe60d07846e9d6aebbc7b3
giggigig <62746515+giggigig@users.noreply.github.com>
2023-05-08 23:52:07 +0900

    print test 01

--- Assets/Scripts/UnityPrintTest.cs
+++ Assets/Scripts/UnityPrintTest.cs
@@ -6,40 +6,61 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System;
+using TMPro;
+using UnityEngine.UI;
 
 namespace LKCSTest
 {
     public class UnityPrintTest : MonoBehaviour
     {
-        private bool useprinterdriver;
+        //private bool useprinterdriver;
         private string m_strPrinter;
 
-        public string inPort;
-        public string inIP;
-        public string inbaudRate;
+        public TextMeshProUGUI logtxt;
+        public TMP_InputField inPort;
+        public TMP_InputField inIP;
+        public TMP_InputField inbaudRate;
 
+        [Header("driver")]
+        public Toggle useprinterdriver;
+        public TMP_InputField indrivername;
+
+
+        // public string inPort;
+        //public string inIP;
+        //public string inbaudRate;
+
+        private void Start()
+        {
+            logtxt.text = "log";
+        }
+
+        private void Update()
+        {
+            
+        }
         public void openButton_Click()
         {
             string port;
             long lResult = 0;
             Int32 baudRate = 0;
 
-            int sIndex = int.Parse(inPort);
-            if (sIndex == 8)
-            {
-                // connect network
-                lResult = LKPrint.OpenPort(inIP, 9100); //포트네임  "USB001" ??
-            }
-            else
-            {
+            //int sIndex = int.Parse(inPort.text); 
+            //if (sIndex == 8)
+            //{
+            //    // connect network
+            //    lResult = LKPrint.OpenPort(inIP.text, 9100); //포트네임  "USB001" ??
+            //}
+
                 // connect other Interface 
-                port = inPort;
-                baudRate = Int32.Parse(inbaudRate);
+                port = inPort.text;
+                baudRate = Int32.Parse(inbaudRate.text);
                 lResult = LKPrint.OpenPort(port, baudRate);
-            }
+
             if (lResult != 0)
             {
-                Debug.LogError("Open Port Failed");
+                Debug.Log("Open Port Failed");
+                logtxt.text="Open Port Failed";
                 return;
             }
             else
@@ -110,13 +131,15 @@ namespace LKCSTest
             TempStr = TempStr + "abc456789012" + "\x0A"; // 인쇄할 바코드 데이타
 
 
-            if (useprinterdriver)
+            if (useprinterdriver.isOn)
             {
                 //m_strPrinter = pDriverNameTextBox.Text.ToString();
                 lResult = LKPrint.OpenPort(m_strPrinter, 1);
                 if (lResult != 0)
                 {
-                    Debug.LogError("OpenPrinter Failed");
+                    Debug.Log("OpenPrinter Failed");
+                    logtxt.text = "OpenPrinter Failed";
+
                     return;
                 }
             }
@@ -133,12 +156,67 @@ namespace LKCSTest
 
             LKPrint.PrintStop();
 
-            if (useprinterdriver)
+            if (useprinterdriver.isOn)
             {
                 lResult = LKPrint.ClosePort();
                 if (lResult != 0)
                 {
-                    Debug.LogError("ClosePrinter Failed!!!");
+                    Debug.Log("ClosePrinter Failed!!!");
+                    logtxt.text = "ClosePrinter Failed!!!";
+                }
+            }
+        }
+        public void printNormalButton_Click()
+        {
+            // TODO: Add your control notification handler code here
+            long lResult;
+
+            if (useprinterdriver.isOn)
+            {
+                m_strPrinter = indrivername.text;
+                lResult = LKPrint.OpenPort(m_strPrinter, 1);
+                if (lResult != 0)
+                {
+                    Debug.Log("OpenPrinter Failed");
+                    logtxt.text = "OpenPrinter Failed";
+
+                    return;
+                }
+            }
+
+            LKPrint.PrintStart();
+
+            LKPrint.PrintBitmap(".\\Logo.bmp", 1, 0, 5, 0); // Print Bitmap
+
+            LKPrint.PrintNormal("\x1b|rATEL (123)-456-7890\n\n\n");
+            LKPrint.PrintNormal("\x1b|cAThank you for coming to our shop!\n");
+            LKPrint.PrintNormal("\x1b|cADate\n\n");
+            LKPrint.PrintNormal("Chicken                             $10.00\n");
+            LKPrint.PrintNormal("Hamburger                           $20.00\n");
+            LKPrint.PrintNormal("Pizza                               $30.00\n");
+            LKPrint.PrintNormal("Lemons                              $40.00\n");
+            LKPrint.PrintNormal("Drink                               $50.00\n");
+            LKPrint.PrintNormal("Excluded tax                       $150.00\n");
+            LKPrint.PrintNormal("\x1b|uCTax(5%)                              $7.50\n");
+            LKPrint.PrintNormal("\x1b|bC\x1b|2CTotal         $157.50\n\n");
+            LKPrint.PrintNormal("Payment                            $200.00\n");
+            LKPrint.PrintNormal("Change                              $42.50\n\n");
+            LKPrint.PrintBarCode("1234567890", 109, 40, 512, 1, 2); // Print Barcode
+
+            LKPrint.PrintBitmap(".\\LUKHAN-logo.bmp", 1, 0, 5, 1); // Print Bitmap
+
+            LKPrint.PrintNormal("\x1b|fP"); // Partial Cut.
+
+            LKPrint.PrintStop();
+
+            if (useprinterdriver.isOn)
+            {
+                lResult = LKPrint.ClosePort();
+                if (lResult != 0)
+                {
+                    Debug.Log("ClosePrinter Failed!!!");
+                    logtxt.text = "ClosePrinter Failed!!!";
+
                 }
             }
         }
